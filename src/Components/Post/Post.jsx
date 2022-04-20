4a1fd481844ad6b15a66345f8bce045a95a96bbe
Michael J <johnson_michael1329@yahoo.com>
2022-04-20 13:17:12 -0500

    Create a template for our Post Component.

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Post = (props) => {
+    return ( 
+        <div>
+            <div>
+                <strong>Mikey Mike </strong>
+            </div>
+            <div>
+                <p>Here is our first status that behaves like Tom from Myspace. I guess Im stuck here XD</p>
+            </div>
+            <div>
+            </div>
+                <button>Like</button>
+                <button>Dislike</button>
+        </div>
+     );
+}
+ 
+export default Post;
